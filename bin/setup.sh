b16a89bd7cd441e9abb1216fdac7903e47457bef
Pat Ludwig <havoc@boldo.com>
2017-01-25 01:00:45 -0600

    Add setup file

---
+++ bin/setup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Setup centralized vim backups
+mkdir -p ~/.vim/undo
+mkdir -p ~/.vim/backups
+mkdir -p ~/.vim/swaps
+
+# Setup vim bundles
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+(
+cd ~/.vim/bundle
+  if [ -e vim-fugitive ]
+  then 
+    pushd vim-fugitive && git pull --quiet && popd
+  else
+    git clone git://github.com/tpope/vim-fugitive.git
+  fi
+	vim -u NONE -c "helptags vim-fugitive/doc" -c q
+)
