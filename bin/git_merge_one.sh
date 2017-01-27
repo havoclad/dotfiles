59146af7e642e04f4523ba5b6c5c7031a54cd146
Pat Ludwig <havoc@boldo.com>
2017-01-27 08:10:28 -0600

    Add --follow to git log to track file renames

--- bin/git_merge_one.sh
+++ bin/git_merge_one.sh
@@ -1,6 +1,6 @@
 mkdir /tmp/mergepatchs
 cd ~/repo/org
 export reposrc=myfile.c #or mydir
-git format-patch -o /tmp/mergepatchs $(git log $reposrc|grep ^commit|tail -1|awk '{print $2}')^..HEAD $reposrc
+git format-patch -o /tmp/mergepatchs $(git log --follow $reposrc|grep ^commit|tail -1|awk '{print $2}')^..HEAD $reposrc
 cd ~/repo/dest
 git am /tmp/mergepatchs/*.patch
