fc639d8b662dbb3a58f540cadbd0cee698227f9c
Pat Ludwig <havoc@boldo.com>
2017-11-10 19:45:23 -0600

    Update to match working copy

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,5 @@
 set -g fish_user_paths "/usr/local/bin" $fish_user_paths
 set fish_function_path $fish_function_path "/usr/local/lib/python2.7/site-packages/powerline/bindings/fish"
 powerline-setup
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
